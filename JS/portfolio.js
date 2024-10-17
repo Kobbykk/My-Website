76ccb22b9196e2807bd67c4ddeccb35a4eb0b04e
Kobby236 <edwinaddai.ea@gmail.com>
2024-10-17 11:06:36 -0400

    Updated Portfolio

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -57,3 +57,28 @@ document.addEventListener(
   },
   false
 );
+
+// Porfolio isotope and filter
+var portfolioIsotope = $(".portfolio-container").isotope({
+  itemSelector: ".portfolio-item",
+  layoutMode: "fitRows",
+});
+
+$("#portfolio-flters li").on("click", function () {
+  $("#portfolio-flters li").removeClass("filter-active");
+  $(this).addClass("filter-active");
+
+  portfolioIsotope.isotope({ filter: $(this).data("filter") });
+});
+
+// Typed Initiate
+if ($('.Video_Text h2').length == 1) {
+  var typed_strings = $('.Video_Text .typed-text').text();
+  var typed = new Typed('.Video_Text h2', {
+    strings: typed_strings.split(", "),
+    typeSpeed: 100,
+    backSpeed: 20,
+    smartBackspace: false,
+    loop: true,
+  });
+}
